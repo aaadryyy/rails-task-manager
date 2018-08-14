3f1b4841541dc2f3c5e866173f67d4cd0f355a12
Adrien Quillet <adrienquillet@gmail.com>
2018-08-14 15:44:49 +0200

    task manager

---
+++ db/migrate/20180814091116_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :details
+      t.boolean :completed, default: false
+
+      t.timestamps
+    end
+  end
+end
