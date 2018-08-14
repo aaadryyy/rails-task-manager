2db681a95de7d4b30ca1b0d790a6647fe7a02576
Adrien Quillet <adrienquillet@gmail.com>
2018-08-14 15:45:14 +0200

    task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,40 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # READ
+  get "tasks", to: "tasks#index"
+
+  # CREATE
+  get "tasks/new", to: "tasks#new"
+  post "/tasks", to: "tasks#create"
+  # EDIT
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # DELETE
+  delete "tasks/:id", to: "tasks#destroy", as: :delete
 end
